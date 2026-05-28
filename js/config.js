a561a03f82ab198df5b7265a3b4651192350aef9
kgafxhh258 <3489419283@qq.com>
2026-05-29 03:08:42 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -109,10 +109,6 @@ const API_SITES = {
         api: 'https://ikunzyapi.com/api.php/provide/vod',
         name: 'iKun资源'
     },
- lsb: {
-         api: 'https://apilsbzy1.com',
-         name: 'lsb资源',
-  },
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
