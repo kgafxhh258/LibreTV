7ff9c5f9debf4ffb72c6476e79fc883197eec013
kgafxhh258 <3489419283@qq.com>
2026-05-29 01:25:13 +0800

    Fix detail URL format in API_SITES configuration

--- js/config.js
+++ js/config.js
@@ -24,7 +24,7 @@ const API_SITES = {
     dyttzy: {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂资源',
-        
+        detail: https://caiji.dyttzyplay.com',
     },
     ruyi: {
         api: 'https://cj.rycjapi.com/api.php/provide/vod',
