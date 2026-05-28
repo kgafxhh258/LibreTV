4c07d63eb92cdd0e25be60989ac73454ae89e090
kgafxhh258 <3489419283@qq.com>
2026-05-29 01:11:58 +0800

    Update API URLs for dyttzy site configuration

--- js/config.js
+++ js/config.js
@@ -22,9 +22,9 @@ const SITE_CONFIG = {
  API站点配置
 const API_SITES = {
     dyttzy: {
-        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod/at',
         name: '电影天堂资源',
-        detail: 'http://caiji.dyttzyapi.com',
+        detail: https://vip.dyttzyplay.com',
     },
     ruyi: {
         api: 'https://cj.rycjapi.com/api.php/provide/vod',
